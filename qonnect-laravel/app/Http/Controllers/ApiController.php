f3305caa5f5f5e3c118d777542a51437b4ddfd33
Thanonchai <thanonchai.khianjai@gmail.com>
2021-08-18 15:24:53 +0700

    main

--- qonnect-laravel/app/Http/Controllers/ApiController.php
+++ qonnect-laravel/app/Http/Controllers/ApiController.php
@@ -10,9 +10,9 @@ class ApiController extends Controller
 {
     public function insert_ssi_historical(Request $request){
         $machine_data = new ssi_historical;
-        $machine_data->name = $request->name;
-        $machine_data->t_message = $request->t_message;
-        $machine_data->person = $request->person;
+        $machine_data->name = $request->his_name;
+        $machine_data->t_message = $request->his_t_message;
+        $machine_data->person = $request->his_person;
         
         $machine_data->save();
 
